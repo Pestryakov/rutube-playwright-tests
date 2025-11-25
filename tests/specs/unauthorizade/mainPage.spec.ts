8a25818fc04d1ff9c088ff51bca38ca73a7b5caa
Maksim <example@email.com>
2025-11-24 21:43:25 -0300

    add main page fixture

--- tests/specs/unauthorizade/mainPage.spec.ts
+++ tests/specs/unauthorizade/mainPage.spec.ts
@@ -1,4 +1,4 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
 test('Open main page', async ({ page }) => {
@@ -6,23 +6,14 @@ test('Open main page', async ({ page }) => {
   await mainPage.open();
 });
 
-test('Checking the accessibility of header elements', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Checking the accessibility of header elements', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
 
-test('Checking the accessibility of tabs categories', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Checking the accessibility of tabs categories', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot();
 });
 
-test('Checking the accessibility of menu', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Checking the accessibility of menu', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
