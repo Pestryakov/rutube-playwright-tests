67c3f80c1e5e37218b530e854d28ad73ae9f40ac
Maksim <example@email.com>
2025-11-24 21:33:09 -0300

    add main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,33 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+  private readonly headerLocator: Locator;
+  private readonly categoriesTabLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page) {
+    super(page);
+    this.headerLocator = this.page.getByRole('banner');
+    this.categoriesTabLocator = this.page.locator('section').filter({
+      hasText: /^ГлавнаяБизнесФильмыСериалыТелешоуСпортБлогерыНовостиМузыкаПодкастыДетямТВ онлайн$/,
+    });
+    this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+  }
+
   async open() {
     await this.page.goto('https://rutube.ru/');
+  }
+
+  async headerHasCorrectAriaSnapshot() {
+    await expect(this.headerLocator).toMatchAriaSnapshot();
+  }
+
+  async categoriesTabsHasCorrectAriaSnapshot() {
+    await expect(this.categoriesTabLocator).toMatchAriaSnapshot();
+  }
+
+  async menuHasCorrectAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
   }
 }
--- tests/specs/unauthorizade/mainPage.spec.ts
+++ tests/specs/unauthorizade/mainPage.spec.ts
@@ -5,3 +5,24 @@ test('Open main page', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.open();
 });
+
+test('Checking the accessibility of header elements', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.closeCookiesAlert();
+  await mainPage.headerHasCorrectAriaSnapshot();
+});
+
+test('Checking the accessibility of tabs categories', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.closeCookiesAlert();
+  await mainPage.categoriesTabsHasCorrectAriaSnapshot();
+});
+
+test('Checking the accessibility of menu', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.closeCookiesAlert();
+  await mainPage.menuHasCorrectAriaSnapshot();
+});
